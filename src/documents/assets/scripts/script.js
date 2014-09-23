bd9edc5c4d484c4260404389bc4ea6d1cc0096f8
Ben Adam <badam@DMK-2.local>
2014-09-22 19:28:26 -0700

    Added the social feature and updated the main logo

--- src/documents/assets/scripts/script.js
+++ src/documents/assets/scripts/script.js
@@ -1,6 +1,13 @@
 /* Your scripts go here */
 $(".content").hide().fadeIn(2000);
 
+$(document).ready(function(){
+  $('#social-hover').click(function(){
+    $('.social-icon').toggle( "slide" );
+  });
+
+});
+
 
 
 
